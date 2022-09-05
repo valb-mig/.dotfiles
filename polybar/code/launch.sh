fd704d3dba38939ce9ef408e2453b8c9df6cd6db
mig <mig@pop-os.localdomain>
2022-09-05 18:28:23 -0300

    🧛

--- polybar/code/launch.sh
+++ polybar/code/launch.sh
@@ -4,5 +4,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar -q right -c "$DIR"/config.ini &
-#polybar -q left -c "$DIR"/config.ini &
+polybar -q left -c "$DIR"/config.ini &
 
