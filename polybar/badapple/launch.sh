f574f08a03ea8517749a109188ebf32f2518de55
Mig <mig@pop-os.localdomain>
2022-10-29 00:53:28 -0300

    🏴

--- polybar/badapple/launch.sh
+++ polybar/badapple/launch.sh
@@ -1,8 +1,7 @@
-DIR="$HOME/.config/polybar/code"
+DIR="$HOME/.config/polybar/badapple"
 killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar -q right -c "$DIR"/config.ini &
 polybar -q left -c "$DIR"/config.ini &
-#polybar -q dock -c "$DIR"/config.ini &
