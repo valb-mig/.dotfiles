cfb8c6359fe398d9414157445ae6b114412250cd
Mig <mig@pop-os.localdomain>
2022-11-13 15:20:29 -0300

    ✖️

--- nvim/lua/cmp-config/init.lua
+++ nvim/lua/cmp-config/init.lua
@@ -62,7 +62,7 @@ vim.g.completeopt="menu,menuone,noselect,noinsert"
   })
 
   -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  --  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
  require('lspconfig').html.setup {
   capabilities = capabilities
