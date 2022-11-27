4bf845501fe5a807a7b002ad260c1f203df916de
Mig <mig@pop-os.localdomain>
2022-11-27 02:26:03 -0300

    😎

--- rofi/powermenu/launcher.sh
+++ rofi/powermenu/launcher.sh
@@ -32,7 +32,7 @@ rofi_cmd() {
 
 # Confirmation CMD
 confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+	rofi -theme-str 'window {location: northeast; anchor: center; fullscreen: false; width: 410px;}' \
 		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
 		-theme-str 'listview {columns: 2; lines: 1;}' \
 		-theme-str 'element-text {horizontal-align: 0.5;}' \
